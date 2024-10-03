3b17876fc202721bcdfa30ba827eed22202b9c74
Vishvajeet shukla <djritikshukla@gmail.com>
2024-10-03 17:12:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
+}
+
+const typeed = new Typed(".text", {
+    strings: ["Frontend Devloper","Video Editor",],
+    typeSpeed: 80,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true,
+})
+
+
+ 
+
+document.getElementById('downloadBtn').addEventListener('click', function() {
+  const link = document.createElement('a');
+  link.href = './media/resume/vishu-resumeg.pdf'; 
+  link.download = 'vishvajeet-resume'; 
+  link.click(); 
+});
